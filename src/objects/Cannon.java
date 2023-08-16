5eddbbe9db48ca4240e768771b477ab1d46f61a1
Hemandr Bhuwanee <hemandr.bhuwanee1@umail.uom.ac.mu>
2023-08-16 23:18:24 +0400

    added comments to objects package

--- src/objects/Cannon.java
+++ src/objects/Cannon.java
@@ -4,23 +4,21 @@ import main.Game;
 
 public class Cannon extends GameObject {
 
-	private int tileY;
+    private int tileY;  // Stores the tile index along the Y-axis where the cannon is located.
 
-	public Cannon(int x, int y, int objType) {
-		super(x, y, objType);
-		tileY = y / Game.TILES_SIZE;
-		initHitbox(40, 26);
-//		hitbox.x -= (int) (1 * Game.SCALE);
-		hitbox.y += (int) (6 * Game.SCALE);
-	}
+    public Cannon(int x, int y, int objType) {
+        super(x, y, objType);  // Call the parent class constructor with provided x, y, and objType.
+        tileY = y / Game.TILES_SIZE;  // Calculate the tile index based on the y-coordinate.
+        initHitbox(40, 26);  // Initialize the hitbox of the cannon with width 40 and height 26.
+        hitbox.y += (int) (6 * Game.SCALE);  // Adjust hitbox position vertically.
+    }
 
-	public void update() {
-		if (doAnimation)
-			updateAnimationTick();
-	}
+    public void update() {
+        if (doAnimation)
+            updateAnimationTick();  // Update animation tick if animation is enabled.
+    }
 
-	public int getTileY() {
-		return tileY;
-	}
-
+    public int getTileY() {
+        return tileY;  // Return the tile index along the Y-axis.
+    }
 }
--- src/objects/Projectile.java
+++ src/objects/Projectile.java
@@ -7,40 +7,40 @@ import main.Game;
 import static utilz.Constants.Projectiles.*;
 
 public class Projectile {
-	private Rectangle2D.Float hitbox;
-	private int dir;
-	private boolean active = true;
+    private Rectangle2D.Float hitbox; // The hitbox of the projectile
+    private int dir; // The direction of the projectile (1 for right, -1 for left)
+    private boolean active = true; // Whether the projectile is currently active or not
 
-	public Projectile(int x, int y, int dir) {
-		int xOffset = (int) (-3 * Game.SCALE);
-		int yOffset = (int) (5 * Game.SCALE);
+    public Projectile(int x, int y, int dir) {
+        int xOffset = (int) (-3 * Game.SCALE); // Offset for the projectile's x-coordinate
+        int yOffset = (int) (5 * Game.SCALE); // Offset for the projectile's y-coordinate
 
-		if (dir == 1)
-			xOffset = (int) (29 * Game.SCALE);
+        if (dir == 1)
+            xOffset = (int) (29 * Game.SCALE); // If direction is right, adjust the x-offset
 
-		hitbox = new Rectangle2D.Float(x + xOffset, y + yOffset, CANNON_BALL_WIDTH, CANNON_BALL_HEIGHT);
-		this.dir = dir;
-	}
+        // Create the projectile's hitbox based on the given position and offsets
+        hitbox = new Rectangle2D.Float(x + xOffset, y + yOffset, CANNON_BALL_WIDTH, CANNON_BALL_HEIGHT);
+        this.dir = dir; // Set the direction of the projectile
+    }
 
-	public void updatePos() {
-		hitbox.x += dir * SPEED;
-	}
+    public void updatePos() {
+        hitbox.x += dir * SPEED; // Update the projectile's x-coordinate based on its direction and speed
+    }
 
-	public void setPos(int x, int y) {
-		hitbox.x = x;
-		hitbox.y = y;
-	}
+    public void setPos(int x, int y) {
+        hitbox.x = x; // Set the projectile's x-coordinate
+        hitbox.y = y; // Set the projectile's y-coordinate
+    }
 
-	public Rectangle2D.Float getHitbox() {
-		return hitbox;
-	}
+    public Rectangle2D.Float getHitbox() {
+        return hitbox; // Get the hitbox of the projectile
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+    public void setActive(boolean active) {
+        this.active = active; // Set whether the projectile is active or not
+    }
 
-	public boolean isActive() {
-		return active;
-	}
-
+    public boolean isActive() {
+        return active; // Check if the projectile is currently active
+    }
 }
