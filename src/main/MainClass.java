9beaa3cf914c23ac32d8ea5eda81e7fb559f353b
Dhruv <dhruv.punchcoory@umail.uom.ac.mu>
2023-08-16 23:55:45 +0400

    added comments to LevelManager

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,8 +1,11 @@
 package main;
 
+// The entry point of the game application.
 public class MainClass {
 
+	// The main method that starts the game.
 	public static void main(String[] args) {
+		// Create a new instance of the Game class to start the game.
 		new Game();
 	}
 
